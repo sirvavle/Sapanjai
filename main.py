6c939a68fc04086d92bcedf03a56d1b0811f1a1c
BuildTools <unconfigured@null.spigotmc.org>
2025-07-18 08:12:08 +0700

    delete uvicorn run

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
 from analysis import analyze_text
-import os
-import uvicorn
 
 app = FastAPI()
 
@@ -18,7 +16,3 @@ def root():
 async def analyze(text_request: TextRequest):
     result = analyze_text(text_request.text)
     return result
-
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 10000))  # Use Render's assigned port or fallback
-    uvicorn.run("main:app", host="0.0.0.0", port=port)
